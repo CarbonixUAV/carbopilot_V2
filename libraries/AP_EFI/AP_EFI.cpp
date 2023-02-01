83f5981952b915861849d8b7e08ba85522932f9e
Pradeep CK <ckpradip@gmail.com>
2023-02-01 14:38:54 +1100

    OV3-493 AP_EFI: Hirth lua binding for fuel consumption
    value is pushed by lua, which in turn gets it from serial data of teensy
    OV3-489 Hirth lua binding for fuel

--- libraries/AP_EFI/AP_EFI.cpp
+++ libraries/AP_EFI/AP_EFI.cpp
@@ -101,6 +101,8 @@ void AP_EFI::init(void)
         gcs().send_text(MAV_SEVERITY_INFO, "Unknown EFI type");
         break;
     }
+
+    lua_fuel_consumed = 0;
 }
 
 // Ask all backends to update the frontend
@@ -108,6 +110,7 @@ void AP_EFI::update()
 {
     if (backend) {
         backend->update();
+        state.estimated_consumed_fuel_volume_cm3 = lua_fuel_consumed;
         log_status();
     }
 }
