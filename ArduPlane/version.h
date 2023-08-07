b292c6b21b8187a4bf9ab9d7cdd101ebfefec8dd
Jono453 <jonathan.loong9328@gmail.com>
2023-08-06 17:59:39 -0700

    OV3-885: Version and Release Notes Ottano Carbopilot 5.0.1

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Ottano Carbopilot V5.0.0"
+#define THISFIRMWARE "Ottano Carbopilot V5.0.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,7,FIRMWARE_VERSION_TYPE_OFFICIAL
