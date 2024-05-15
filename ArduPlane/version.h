2e77acc9e9731ebb7a941431178a69a7fb95ccaf
Lokesh-Ramina <loki077@gmail.com>
2024-05-15 16:23:06 -0700

    Volanti: 4.6.0 Release Notes & Version
    SW-196

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Volanti Carbopilot V4.5.0"
+#define THISFIRMWARE "Volanti Carbopilot V4.6.0"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,1,FIRMWARE_VERSION_TYPE_OFFICIAL
